4e382d0139b75567b52b55431b7c5cc35ff66c65
George-25 <georgemallory26@gmail.com>
2025-05-18 17:50:56 -0400

    added some code in js

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 
 let todos = [];
 
+
+// Grab elements
 const input = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const listElement = document.getElementById("todo-list");
+
+
+// Add task function
+const addTodo = () => {
+
+  const text = input.value.trim();
+  if (!text) {
+    return alert("Please enter a task")
+  }
+
+  // ArrayLisk.add()
+
+  todos.push({ text, done: false });
+  input.value = "";
+
+
+}
+
+
+addBtn.addEventListener('click', addTodo);
