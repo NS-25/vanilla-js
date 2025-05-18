da9f27692003b46e2d9889cb6dbfbdceb6f4358e
George-25 <georgemallory26@gmail.com>
2025-05-18 18:14:17 -0400

    added all necessary functions

--- script.js
+++ script.js
@@ -29,8 +29,34 @@ const toggleTodo = (index) => {
   todos[index].done = !todos[index];
   renderTodos();
 }
-///// 
+///// Remove task
+const removeTodo = (index) => {
+  // ArrayList.remove()
+  todos.splice(index, 1);
+  renderTodos();
+}
 
+//// Render list to the DOM
 
+const renderTodos = () => {
+  listElement.innerHTML = '';
+  todos.forEach((todo, i) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span style="text-decoration:${todo.done ? 'line-through' : 'none'}">
+        ${todo.text}
+      </span>
+      <button onclick="toggleTodo(${i})">
+        ${todo.done ? 'Undo' : 'Done'}
+      </button>
+      <button onclick="removeTodo(${i})">🗑️</button>
+    `;
+    listElement.appendChild(li);
+  });
+}
 
+// Wire up button
 addBtn.addEventListener('click', addTodo);
+
+// Initial render (empty);
+renderTodos();
